9420850bf9f8f1db48a6100734ef7c2e6cb53286
Wolf <163815746+WolfInBox@users.noreply.github.com>
2024-10-22 11:17:45 +0400

    add right input pressed detection

--- Assets/Core/_Scripts/Game/Minigames/ArmWrestling/ArmWrestlingbehavior.cs
+++ Assets/Core/_Scripts/Game/Minigames/ArmWrestling/ArmWrestlingbehavior.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Windows;
 
 namespace RapidPrototyping.TicTacMix.ArmWresling
 {
     public class ArmWrestlingbehavior : MonoBehaviour
     {
-        private int m_playerIndex;
+        [SerializeField] private int m_playerIndex;
         private enum Inputs
         {
             UP,
@@ -20,12 +22,13 @@ namespace RapidPrototyping.TicTacMix.ArmWresling
         private Inputs m_p2Inputs;
 
         private int m_Score = 0;
+        private int m_beforChange;
 
         void Start()
         {
             InitDictionary();
-            
-            m_p1Inputs = Inputs.LEFT;
+
+            m_p1Inputs = Inputs.UP;
             m_p2Inputs = Inputs.RIGHT;
         }
 
@@ -34,10 +37,18 @@ namespace RapidPrototyping.TicTacMix.ArmWresling
             if (m_playerIndex == 0)
             {
                 var input = InputManager.InputActions.P1Gameplay;
+                if (input.Movement.WasPressedThisFrame())
+                {
+                    var movement = input.Movement.ReadValue<Vector2>();
+                    ValidInput(m_p1Inputs, movement);
+                }
+                
             }
             else
             {
                 var input = InputManager.InputActions.P2Gameplay;
+                var movement = input.Movement.ReadValue<Vector2>();
+                ValidInput(m_p2Inputs, movement);
             }
         }
         void InitDictionary()
@@ -46,6 +57,49 @@ namespace RapidPrototyping.TicTacMix.ArmWresling
             m_dictionary.Add(Inputs.DOWN, new Vector2(0, -1));
             m_dictionary.Add(Inputs.LEFT, new Vector2(-1, 0));
             m_dictionary.Add(Inputs.RIGHT, new Vector2(1, 0));
+        }
+
+        void ChangeInput(int current, Inputs currentPlayerInput)
+        {
+            if (current == 0)
+            {
+                int newInput = Random.Range(0, 5);
+                if (newInput == 0)
+                {
+                    currentPlayerInput = Inputs.LEFT;
+                }
+                if (newInput == 1)
+                {
+                    currentPlayerInput = Inputs.RIGHT;
+                }
+                if (newInput == 2)
+                {
+                    currentPlayerInput = Inputs.UP;
+                }
+                if (newInput == 3)
+                {
+                    currentPlayerInput = Inputs.DOWN;
+                }
+                m_beforChange = Random.Range(5, 10);
+            }
+        }
+        void ValidInput(Inputs currentInput, Vector2 playerInput)
+        {
+            Vector2 value = new Vector2(0, 0);
+            int count = 0;
+            foreach (Inputs inputs in m_dictionary.Keys)
+            {
+                if (inputs == currentInput)
+                {
+                    value = m_dictionary.ElementAt(count).Value;
+                }
+                count++;
+            }
+
+            if (playerInput == value)
+            {
+                Debug.Log("yes right thing");
+            }
         }
     }
 }
