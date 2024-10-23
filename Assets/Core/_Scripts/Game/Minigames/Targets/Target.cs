4aa6ffa88ea1ebe49e3dbb4b1c909667363a4de1
Luckius_ <herocreepyfr@gmail.com>
2024-10-23 14:17:27 +0400

    Implement effects + target animation
    Targets can turn around and then explode to let go and give the space to other targets instead

--- Assets/Core/_Scripts/Game/Minigames/Targets/Target.cs
+++ Assets/Core/_Scripts/Game/Minigames/Targets/Target.cs
@@ -1,3 +1,5 @@
+using CartoonFX;
+using System.Collections;
 using UnityEngine;
 
 namespace RapidPrototyping.TicTacMix.Targets
@@ -5,14 +7,49 @@ namespace RapidPrototyping.TicTacMix.Targets
     [RequireComponent(typeof(Collider))]
     public class Target : MonoBehaviour
     {
+        [SerializeField] private float m_lifetime = 3f;
         [SerializeField] private int m_score = 1;
         [SerializeField] private Destructible m_destructible;
+
+        [Space]
+
+        [SerializeField] private CFXR_Effect m_disableEffect;
+        [SerializeField] private CFXR_Effect m_impactEffect;
 
         private Collider m_collider;
 
         private void Start()
         {
             m_collider = GetComponent<Collider>();
+            Invoke(nameof(Disable), m_lifetime);
+        }
+
+        private void Disable()
+        {
+            m_collider.enabled = false;
+            StartCoroutine(nameof(Rotate));
+        }
+
+        private IEnumerator Rotate()
+        {
+            Quaternion baseRotation = transform.rotation;
+            Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles
+                + Vector3.up * 180f);
+
+            float time = 0f;
+            const float duration = 0.6f;
+            while (time < duration)
+            {
+                time += Time.deltaTime;
+                yield return null;
+                transform.rotation = Quaternion.Lerp(baseRotation,
+                    targetRotation, time / duration);
+            }
+
+            transform.rotation = targetRotation;
+            yield return null;
+            Instantiate(m_disableEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
         }
 
         public virtual void OnProjectileHit(int playerIndex)
@@ -25,6 +62,8 @@ namespace RapidPrototyping.TicTacMix.Targets
             if (collision.gameObject.TryGetComponent<Projectile>(out var projectile))
             {
                 OnProjectileHit(projectile.PlayerIndex);
+
+                Instantiate(m_impactEffect, collision.contacts[0].point, Quaternion.identity);
 
                 if (m_destructible)
                 {
