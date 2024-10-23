83925e097c1312a54029fe4e80342938654309a5
Luckius_ <herocreepyfr@gmail.com>
2024-10-23 08:55:44 +0400

    Add comments to mystery doors game manager

--- Assets/Core/_Scripts/Game/Minigames/Mystery Doors/GameManager.cs
+++ Assets/Core/_Scripts/Game/Minigames/Mystery Doors/GameManager.cs
@@ -11,21 +11,26 @@ namespace RapidPrototyping.TicTacMix.MysteryDoors
 
         [SerializeField] private float m_floorHeight = 3f;
 
-        [SerializeField] private Transform m_tower;
-        [SerializeField] private Floor[] m_floorPrefabs;
-        [SerializeField] private Ground m_groundFloor;
+        [Space]
+
         [SerializeField] private Player m_playerPrefab;
+        [SerializeField] private Transform m_tower;
+        [SerializeField] private Ground m_groundFloor;
+        [SerializeField] private Floor[] m_floorPrefabs;
 
         private Floor m_topFloor;
 
         private void Start()
         {
+            // Generate the tower and spawn the players in the top floor
             GenerateTower();
             SetupPlayers();
         }
 
         private void GenerateTower()
         {
+            // This code only executes if at least one floor prefab has been
+            // given to the game manager
             if (m_floorPrefabs == null)
                 return;
 
@@ -36,33 +41,47 @@ namespace RapidPrototyping.TicTacMix.MysteryDoors
             Vector3 position = m_groundFloor.transform.position;
             for (int i = 0; i < 5; i++)
             {
+                // Get this floor position
                 position += Vector3.up * m_floorHeight;
+
+                // Instantiate a new floor
                 var prefab = m_floorPrefabs[0];
                 var floor = Instantiate(prefab, position, Quaternion.identity);
                 floor.transform.parent = m_tower;
                 floor.name = $"Floor_{i + 1}";
 
-                if (previousFloor != null)
+                // This piece of code will link each stage with each other
+                // to let the player navigate in between them with doors.
+                if (previousFloor != null) // If a floor was generated before this one...
                 {
                     floor.previousStage = previousFloor;
                 }
-                else
+                else // Else, set the previous stage as the ground floor
                 {
                     floor.previousStage = m_groundFloor;
                 }
 
+                // Mark this generated floor as the predecessor for the next one
                 previousFloor = floor;
             }
 
+            // Mark the last generated floor as the top one
             m_topFloor = previousFloor;
         }
 
         private void SetupPlayers()
         {
+            // This code only executes if a player prefab has been given to
+            // the game manager
+            if (m_playerPrefab == null)
+                return;
+
+            // Instantiate the first player on the left side of the top floor
             var playerOne = Instantiate(m_playerPrefab, Vector3.zero, Quaternion.identity);
             playerOne.Initialize(0, m_topFloor);
             m_camera.AddTarget(playerOne.transform);
 
+            // Instantiate the second player on the right side of the top floor
             var playerTwo = Instantiate(m_playerPrefab, Vector3.zero, Quaternion.identity);
             playerTwo.Initialize(1, m_topFloor);
             m_camera.AddTarget(playerTwo.transform);
