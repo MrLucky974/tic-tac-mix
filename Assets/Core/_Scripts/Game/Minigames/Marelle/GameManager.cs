c9d6efc39b80f654ee49e55c9adf997507365677
JoltL <laurie.ligdamis@gmail.com>
2024-10-23 15:47:30 +0400

    GameManager

---
+++ Assets/Core/_Scripts/Game/Minigames/Marelle/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RapidPrototyping.TicTacMix.Marelle
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] Player _player;
+    }
+}
