6636f4ad65aef4563131e21749149eaa68ada746
Luckius_ <herocreepyfr@gmail.com>
2024-11-12 08:30:32 +0400

    Integrate trains minigame

--- Assets/Core/_Scripts/Game/Minigames/Trains/GameManager.cs
+++ Assets/Core/_Scripts/Game/Minigames/Trains/GameManager.cs
@@ -1,12 +1,17 @@
+using LuckiusDev.Utils;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace RapidPrototyping.TicTacMix.Trains
 {
     public class GameManager : MonoBehaviour
     {
+        public const int TIE_INDEX = -1;
+        public const int PLAYER_ONE_INDEX = 0;
+        public const int PLAYER_TWO_INDEX = 1;
+
         [Header("CHRONO")]
         //CHRONO OF THE GAME
         private float _time;
@@ -29,15 +34,25 @@ namespace RapidPrototyping.TicTacMix.Trains
         [SerializeField] private TMP_Text _countdownText;
         public bool _canSpawn;
 
+        private CountdownTimer m_timer;
+
         private void Start()
         {
             _player = FindObjectsOfType<PlayerController>();
             _canSpawn = false;
             StartCoroutine(Countdown());
+
+            m_timer = new CountdownTimer(3f);
+            m_timer.OnTimerStop += () =>
+            {
+                LoadGameplaySceneForNextTurn();
+            };
         }
 
         private void Update()
         {
+            m_timer.Tick(Time.unscaledDeltaTime);
+
             if (_canSpawn)
             {
                 _time += Time.deltaTime;
@@ -56,9 +71,8 @@ namespace RapidPrototyping.TicTacMix.Trains
             }
         }
 
-        IEnumerator Countdown()
+        private IEnumerator Countdown()
         {
-
             _countdownText.gameObject.SetActive(true);
 
             _countdownText.text = "3";
@@ -91,12 +105,13 @@ namespace RapidPrototyping.TicTacMix.Trains
                 {
                     _text.GetComponent<TMP_Text>().color = _colors[1];
                     _text.text = "Victory: Player O";
-
+                    MarkWinningSymbol(PLAYER_TWO_INDEX);
                 }
                 else
                 {
                     _text.GetComponent<TMP_Text>().color = _colors[0];
                     _text.text = "Victory: Player X";
+                    MarkWinningSymbol(PLAYER_ONE_INDEX);
                 }
             }
             else if (_player[0]._score < _player[1]._score)
@@ -106,32 +121,38 @@ namespace RapidPrototyping.TicTacMix.Trains
                 {
                     _text.GetComponent<TMP_Text>().color = _colors[0];
                     _text.text = "Victory: Player X";
+                    MarkWinningSymbol(PLAYER_ONE_INDEX);
 
                 }
                 else
                 {
                     _text.GetComponent<TMP_Text>().color = _colors[1];
                     _text.text = "Victory: Player O";
+                    MarkWinningSymbol(PLAYER_TWO_INDEX);
 
                 }
             }
-
             else if (_player[0]._score == _player[1]._score)
             {
                 _text.text = "Victory: Tie";
+                MarkWinningSymbol(TIE_INDEX);
             }
 
             ShowScoreText();
             Time.timeScale = 0f;
+            m_timer.Start();
         }
 
         public void Tie()
         {
             _victoryPanel.SetActive(true);
             _text.text = "Victory: Tie";
+            MarkWinningSymbol(TIE_INDEX);
             ShowScoreText();
             Time.timeScale = 0f;
+            m_timer.Start();
         }
+
         public void PlayerDead(bool isPlayerO)
         {
             _victoryPanel.SetActive(true);
@@ -140,21 +161,64 @@ namespace RapidPrototyping.TicTacMix.Trains
             {
                 _text.GetComponent<TMP_Text>().color = _colors[0];
                 _text.text = "Victory: Player X";
+                MarkWinningSymbol(PLAYER_ONE_INDEX);
             }
             else
             {
                 _text.GetComponent<TMP_Text>().color = _colors[1];
                 _text.text = "Victory: Player O";
+                MarkWinningSymbol(PLAYER_TWO_INDEX);
             }
 
             ShowScoreText();
             Time.timeScale = 0f;
+            m_timer.Start();
         }
 
-        void ShowScoreText()
+        private void ShowScoreText()
         {
             _scoreText[0].text = _player[0]._score.ToString();
             _scoreText[1].text = _player[1]._score.ToString();
+        }
+
+        /// <summary>
+        /// Advances the game by changing the player's turn and loading the main gameplay scene.
+        /// </summary>
+        public static void LoadGameplaySceneForNextTurn()
+        {
+            // Change the active player turn in GameDataHandler.
+            GameDataHandler.ChangeTurn();
+
+            // Set the time scale back to 1
+            Time.timeScale = 1f;
+
+            // Load the main gameplay scene based on the configured scene reference.
+            SceneManager.LoadScene(GameDataHandler.MainGameplaySceneReference);
+        }
+
+        /// <summary>
+        /// Marks the winning symbol on the grid at the specified position when a game ends.
+        /// </summary>
+        /// <param name="winIndex">
+        /// The index indicating the winning player:
+        /// - 0 for player using "Cross"
+        /// - 1 for player using "Circle"
+        /// - -1 if there is no winner (in which case, no symbol is placed).
+        /// </param>
+        public static void MarkWinningSymbol(int winIndex)
+        {
+            // Retrieve the current grid position from GameDataHandler.
+            var gridPosition = GameDataHandler.DataHolder.GridPosition;
+
+            // Check if there is a winning player.
+            if (winIndex != TIE_INDEX)
+            {
+                // Determine the symbol to place based on the winning player's index.
+                var symbol = winIndex == PLAYER_ONE_INDEX ? GridManager.Symbol.Cross : GridManager.Symbol.Circle;
+
+                // Place the winning symbol on the grid at the designated position.
+                GridManager.PlaceSymbol(symbol, gridPosition.x, gridPosition.y);
+            }
         }
     }
 }
