4decf81bb1bf07781cabb11be1290adc5ed1f278
Luckius_ <herocreepyfr@gmail.com>
2024-11-13 14:37:34 +0400

    Implement visual effects in mystery doors

--- Assets/Core/_Scripts/Game/Minigames/Mystery Doors/Door.cs
+++ Assets/Core/_Scripts/Game/Minigames/Mystery Doors/Door.cs
@@ -1,15 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 namespace RapidPrototyping.TicTacMix.MysteryDoors
 {
     public class Door : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private SpriteRenderer m_spriteRenderer;
+
+        [Header("Animations")]
+        [SerializeField] private Sprite m_closedSprite;
+        [SerializeField] private Sprite m_openedSprite;
+
+        [Header("User Interface")]
+        [SerializeField] private CanvasGroup m_p1InputPromptCanvasGroup;
+        [SerializeField] private CanvasGroup m_p2InputPromptCanvasGroup;
+
         protected Player m_playerOne, m_playerTwo;
         private bool m_isTrapped;
 
         public void Initialize(bool trapped)
         {
             m_isTrapped = trapped;
+            m_spriteRenderer.sprite = m_closedSprite;
+
+            m_p1InputPromptCanvasGroup.alpha = 0f;
+            m_p1InputPromptCanvasGroup.GetComponent<RectTransform>().localScale = Vector3.zero;
+            m_p1InputPromptCanvasGroup.gameObject.SetActive(false);
+
+            m_p2InputPromptCanvasGroup.alpha = 0f;
+            m_p2InputPromptCanvasGroup.GetComponent<RectTransform>().localScale = Vector3.zero;
+            m_p2InputPromptCanvasGroup.gameObject.SetActive(false);
         }
 
         protected virtual void Update()
@@ -23,19 +44,20 @@ namespace RapidPrototyping.TicTacMix.MysteryDoors
                 var movement = input.Movement.ReadValue<Vector2>().y;
                 if (input.Movement.WasPressedThisFrame() && movement > 0f)
                 {
-                    if (m_isTrapped)
-                    {
-                        var topStage = GameManager.TopFloor;
-                        m_playerOne.SetNewStage(topStage);
-                    }
-                    else
-                    {
-                        if (m_playerOne.currentStage is Floor floor)
-                        {
-                            var nextStage = floor.previousStage;
-                            m_playerOne.SetNewStage(nextStage);
-                        }
-                    }
+                    //if (m_isTrapped)
+                    //{
+                    //    var topStage = GameManager.TopFloor;
+                    //    m_playerOne.SetNewStage(topStage);
+                    //}
+                    //else
+                    //{
+                    //    if (m_playerOne.currentStage is Floor floor)
+                    //    {
+                    //        var nextStage = floor.previousStage;
+                    //        m_playerOne.SetNewStage(nextStage);
+                    //    }
+                    //}
+                    StartCoroutine(OpenDoor(m_playerOne));
                 }
             }
 
@@ -45,21 +67,47 @@ namespace RapidPrototyping.TicTacMix.MysteryDoors
                 var movement = input.Movement.ReadValue<Vector2>().y;
                 if (input.Movement.WasPressedThisFrame() && movement > 0f)
                 {
-                    if (m_isTrapped)
-                    {
-                        var topStage = GameManager.TopFloor;
-                        m_playerTwo.SetNewStage(topStage);
-                    }
-                    else
-                    {
-                        if (m_playerTwo.currentStage is Floor floor)
-                        {
-                            var nextStage = floor.previousStage;
-                            m_playerTwo.SetNewStage(nextStage);
-                        }
-                    }
+                    //if (m_isTrapped)
+                    //{
+                    //    var topStage = GameManager.TopFloor;
+                    //    m_playerTwo.SetNewStage(topStage);
+                    //}
+                    //else
+                    //{
+                    //    if (m_playerTwo.currentStage is Floor floor)
+                    //    {
+                    //        var nextStage = floor.previousStage;
+                    //        m_playerTwo.SetNewStage(nextStage);
+                    //    }
+                    //}
+                    StartCoroutine(OpenDoor(m_playerTwo));
                 }
             }
+        }
+
+        private IEnumerator OpenDoor(Player player)
+        {
+            m_spriteRenderer.sprite = m_openedSprite;
+            player.SetAnimationState(Player.AnimationState.OPEN_DOOR);
+
+            yield return new WaitForSecondsRealtime(0.1f);
+
+            if (m_isTrapped)
+            {
+                var topStage = GameManager.TopFloor;
+                player.SetNewStage(topStage);
+            }
+            else
+            {
+                if (player.currentStage is Floor floor)
+                {
+                    var nextStage = floor.previousStage;
+                    player.SetNewStage(nextStage);
+                }
+            }
+
+            m_spriteRenderer.sprite = m_closedSprite;
+            player.SetAnimationState(Player.AnimationState.IDLE);
         }
 
         protected virtual void OnTriggerEnter2D(Collider2D collision)
@@ -68,11 +116,19 @@ namespace RapidPrototyping.TicTacMix.MysteryDoors
             {
                 if (player.PlayerIndex == 0)
                 {
+                    m_p1InputPromptCanvasGroup.gameObject.SetActive(true);
+                    m_p1InputPromptCanvasGroup.alpha = 1f;
+                    m_p1InputPromptCanvasGroup.GetComponent<RectTransform>().localScale = Vector3.one;
+
                     m_playerOne = player;
                 }
 
                 if (player.PlayerIndex == 1)
                 {
+                    m_p2InputPromptCanvasGroup.gameObject.SetActive(true);
+                    m_p2InputPromptCanvasGroup.alpha = 1f;
+                    m_p2InputPromptCanvasGroup.GetComponent<RectTransform>().localScale = Vector3.one;
+
                     m_playerTwo = player;
                 }
             }
@@ -84,11 +140,19 @@ namespace RapidPrototyping.TicTacMix.MysteryDoors
             {
                 if (player.PlayerIndex == 0)
                 {
+                    m_p1InputPromptCanvasGroup.alpha = 0f;
+                    m_p1InputPromptCanvasGroup.GetComponent<RectTransform>().localScale = Vector3.zero;
+                    m_p1InputPromptCanvasGroup.gameObject.SetActive(false);
+
                     m_playerOne = null;
                 }
 
                 if (player.PlayerIndex == 1)
                 {
+                    m_p2InputPromptCanvasGroup.alpha = 0f;
+                    m_p2InputPromptCanvasGroup.GetComponent<RectTransform>().localScale = Vector3.zero;
+                    m_p2InputPromptCanvasGroup.gameObject.SetActive(false);
+
                     m_playerTwo = null;
                 }
             }
--- Assets/Core/_Scripts/Game/Minigames/Mystery Doors/Player.cs
+++ Assets/Core/_Scripts/Game/Minigames/Mystery Doors/Player.cs
@@ -1,22 +1,44 @@
+using LuckiusDev.Utils;
 using UnityEngine;
 
 namespace RapidPrototyping.TicTacMix.MysteryDoors
 {
     public class Player : MonoBehaviour
     {
-        [SerializeField] private float m_speed = 0.2f;
+        public enum AnimationState
+        {
+            IDLE = 0,
+            WALK = 1,
+            OPEN_DOOR = 2,
+        }
+
+        [Header("Settings")]
+        [SerializeField] private int m_playerIndex;
 
         [Space]
-
-        [SerializeField] private int m_playerIndex;
-        [SerializeField] private SpriteRenderer m_spriteRenderer;
 
         [SerializeField] private Color m_blueColor;
         [SerializeField] private Color m_redColor;
 
-        [HideInInspector] public Stage currentStage;
+        [Space]
 
+        [SerializeField] private float m_speed = 0.2f;
+
+        [Header("References")]
+        [SerializeField] private SpriteRenderer m_spriteRenderer;
+
+        [Header("Animations")]
+        [SerializeField] private Sprite m_idleSprite;
+        [SerializeField] private Sprite[] m_walkSprites;
+        [SerializeField] private float m_walkFramePerSecond = 5f;
+        [SerializeField] private Sprite m_doorSprite;
+
+        [HideInInspector] public Stage currentStage;
         private float m_offset;
+
+        private AnimationState m_currentAnimationState = AnimationState.IDLE;
+        private int m_animationSpriteIndex = 0;
+        private float m_currentFrameDuration = 0f;
 
         public void Initialize(int playerIndex, Stage stage)
         {
@@ -29,7 +51,53 @@ namespace RapidPrototyping.TicTacMix.MysteryDoors
             transform.position = currentStage.GetPosition(m_offset);
             m_spriteRenderer.color = playerIndex == 0 ? m_blueColor : m_redColor;
 
+            SetAnimationState(AnimationState.IDLE);
+
             GameManager.Instance.OnGameEnded += HandleGameEnd;
+        }
+
+        public AnimationState GetAnimationState()
+        {
+            return m_currentAnimationState;
+        }
+
+        public void SetAnimationState(AnimationState state)
+        {
+            if (state == m_currentAnimationState)
+            {
+                return;
+            }
+
+            m_currentAnimationState = state;
+
+            m_currentFrameDuration = 0f;
+            m_animationSpriteIndex = 0;
+        }
+
+        private void UpdateAnimation(float deltaTime)
+        {
+            m_currentFrameDuration += deltaTime;
+
+            switch (m_currentAnimationState)
+            {
+                case AnimationState.IDLE:
+                    m_spriteRenderer.sprite = m_idleSprite;
+                    break;
+                case AnimationState.WALK:
+                    float walkFrameDelay = 1f / m_walkFramePerSecond;
+                    if (m_currentFrameDuration > walkFrameDelay)
+                    {
+                        m_animationSpriteIndex = JMath.WrapValue(0,
+                            m_walkSprites.Length - 1, m_animationSpriteIndex + 1);
+                        m_currentFrameDuration = 0f;
+                    }
+
+                    m_spriteRenderer.sprite = m_walkSprites[m_animationSpriteIndex];
+                    break;
+                case AnimationState.OPEN_DOOR:
+                    m_spriteRenderer.sprite = m_doorSprite;
+                    break;
+            }
         }
 
         private void HandleGameEnd(GameData data)
@@ -47,6 +115,8 @@ namespace RapidPrototyping.TicTacMix.MysteryDoors
         private void Update()
         {
             var deltaTime = Time.deltaTime;
+            UpdateAnimation(deltaTime);
+
             float normalizedSpeed = m_speed / currentStage.GetDistance();
 
             if (m_playerIndex == 0)
@@ -58,6 +128,17 @@ namespace RapidPrototyping.TicTacMix.MysteryDoors
                 if (movement != 0f)
                 {
                     m_spriteRenderer.flipX = movement < 0f;
+                    if (m_currentAnimationState != AnimationState.OPEN_DOOR)
+                    {
+                        SetAnimationState(AnimationState.WALK);
+                    }
+                }
+                else
+                {
+                    if (m_currentAnimationState != AnimationState.OPEN_DOOR)
+                    {
+                        SetAnimationState(AnimationState.IDLE);
+                    }
                 }
             }
             else if (m_playerIndex == 1)
@@ -68,6 +149,18 @@ namespace RapidPrototyping.TicTacMix.MysteryDoors
                 if (movement != 0f)
                 {
                     m_spriteRenderer.flipX = movement < 0f;
+
+                    if (m_currentAnimationState != AnimationState.OPEN_DOOR)
+                    {
+                        SetAnimationState(AnimationState.WALK);
+                    }
+                }
+                else
+                {
+                    if (m_currentAnimationState != AnimationState.OPEN_DOOR)
+                    {
+                        SetAnimationState(AnimationState.IDLE);
+                    }
                 }
             }
 
