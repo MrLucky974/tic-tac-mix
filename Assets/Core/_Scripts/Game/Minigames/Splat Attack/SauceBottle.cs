a762f52888982868b2b2d75ab9e48b7279cb69c4
Luckius_ <github@luckius.fr>
2024-11-20 21:27:36 +0400

    Refactor input management on splat attack

--- Assets/Core/_Scripts/Game/Minigames/Splat Attack/SauceBottle.cs
+++ Assets/Core/_Scripts/Game/Minigames/Splat Attack/SauceBottle.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 namespace RapidPrototyping.TicTacMix.SplatAttack
 {
-    public class SauceBottle : MonoBehaviour
+    public class SauceBottle : MonoBehaviour, IPlayerControls
     {
         private const float SPEED = 10f;
         private const float RESPONSE = 25f;
 
         [Header("Settings")]
         [SerializeField] private PlayerIdentifier m_identifier;
+        [SerializeField] private PlayerInput m_playerInput;
 
         [Header("References")]
         [SerializeField] private Splat m_splatPrefab;
@@ -23,20 +26,32 @@ namespace RapidPrototyping.TicTacMix.SplatAttack
         [Header("Audio")]
         [SerializeField] private AudioClip[] m_splatSounds;
 
-        private GameInputActions m_inputActions;
-
-        private bool m_primaryRequested;
-        private Vector2 m_requestedMovement;
-
         private Vector2 m_velocity;
 
         private float m_springVelocity;
         private float m_springDisplacement;
         private Vector3 m_defaultScale;
 
+        #region Input Variables
+
+        private Vector2 m_movementInput;
+        private bool m_primaryPressedThisFrame;
+
+        #endregion
+
         private void Start()
         {
-            m_inputActions = InputManager.InputActions; // Grab a reference to the input
+            m_playerInput.SwitchCurrentControlScheme(m_playerInput.defaultControlScheme);
+            InputUser.PerformPairingWithDevice(Keyboard.current, m_playerInput.user, InputUserPairingOptions.None);
+            InputUser.PerformPairingWithDevice(Mouse.current, m_playerInput.user, InputUserPairingOptions.None);
+            if (m_identifier == PlayerIdentifier.PLAYER_TWO)
+            {
+                if (Gamepad.all.Count >= 1)
+                {
+                    var gamepad = Gamepad.all[0];
+                    InputUser.PerformPairingWithDevice(gamepad, m_playerInput.user, InputUserPairingOptions.None);
+                }
+            }
 
             // Set the player color based on the identifier
             var color = GameManager.GetColor(m_identifier);
@@ -50,9 +65,9 @@ namespace RapidPrototyping.TicTacMix.SplatAttack
             UpdateDampedOscillator();
 
             // Update sauce bottle velocity
-            if (m_requestedMovement.magnitude > 0f)
+            if (m_movementInput.magnitude > 0f)
             {
-                var targetVelocity = m_requestedMovement.normalized
+                var targetVelocity = m_movementInput.normalized
                     * SPEED * Time.deltaTime;
 
                 m_velocity = Vector2.Lerp(m_velocity,
@@ -74,10 +89,8 @@ namespace RapidPrototyping.TicTacMix.SplatAttack
             if (GameManager.GameRunning is false)
                 return;
 
-            UpdateInput();
-
             // If primary action is pressed, spawn a new splat prefab
-            if (m_primaryRequested)
+            if (m_primaryPressedThisFrame)
             {
                 var instance = m_splatPrefab.Create(m_identifier);
                 instance.transform.position = m_spawnPoint.position;
@@ -89,9 +102,15 @@ namespace RapidPrototyping.TicTacMix.SplatAttack
                     var sound = m_splatSounds.PickRandomUnity();
                     SoundManager.Play(sound);
                 }
-
-                m_primaryRequested = false;
             }
+        }
+
+        private void LateUpdate()
+        {
+            // Prevent players from going outside the game bounds
+            transform.ClampPositionToRect(GameManager.WorldRect);
+
+            m_primaryPressedThisFrame = false;
         }
 
         private void UpdateDampedOscillator()
@@ -102,37 +121,14 @@ namespace RapidPrototyping.TicTacMix.SplatAttack
             m_renderer.transform.localScale = m_defaultScale + new Vector3(-m_springDisplacement, m_springDisplacement) * m_scaleFactor;
         }
 
-        private void LateUpdate()
+        public void OnMovement(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
         {
-            // Prevent players from going outside the game bounds
-            transform.ClampPositionToRect(GameManager.WorldRect);
+            m_movementInput = ctx.ReadValue<Vector2>();
         }
 
-        private void UpdateInput()
+        public void OnPrimary(UnityEngine.InputSystem.InputAction.CallbackContext ctx)
         {
-            switch (m_identifier)
-            {
-                case PlayerIdentifier.PLAYER_ONE:
-                    var p1Input = m_inputActions.P1Gameplay;
-
-                    m_requestedMovement = p1Input.Movement.ReadValue<Vector2>();
-
-                    if (p1Input.Primary.WasPressedThisFrame())
-                    {
-                        m_primaryRequested = true;
-                    }
-                    break;
-                case PlayerIdentifier.PLAYER_TWO:
-                    var p2Input = m_inputActions.P2Gameplay;
-
-                    m_requestedMovement = p2Input.Movement.ReadValue<Vector2>();
-
-                    if (p2Input.Primary.WasPressedThisFrame())
-                    {
-                        m_primaryRequested = true;
-                    }
-                    break;
-            }
+            m_primaryPressedThisFrame |= ctx.action.WasPressedThisFrame();
         }
     }
 }
